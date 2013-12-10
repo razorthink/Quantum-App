3fa19a46136e7b1d7d872fda6326f17fde727075
Sandeep S Kumar <sandeep.kumar@razorthink.com>
2013-12-10 10:58:09 +0530

    New commit

---
+++ fixture.js
@@ -0,0 +1,19 @@
+var fixture = {
+    users: [{
+		fname: "Sandeep",
+		lname: "S Kumar",
+		email: "sandeep.kumar@razorthink.com",
+		UID: 1,
+		pswd: "123456",
+		avatar: "images/avatars/avatar.jpeg"
+	}, {
+		fname: "Nanha",
+		lname: "Ayesha",
+		email: "nanha.ab@razorthink.com",
+		UID: 2,
+		pswd: "123456",
+		avatar: "images/avatars/avatar.jpeg"
+	}]
+};
+
+module.exports = fixture;
--- server.js
+++ server.js
@@ -2,12 +2,35 @@ var	express	= require('express'),
 	server	= express(),
 	child	= require('child_process'),
 	fs		= require('fs');
-
-var	scriptDir	= "render_scripts",
-	imageDir	= "render_images";
+	fixture	= require('./fixture');
+	_		= require('underscore');
 
 server.use(express.static(__dirname + '/app'));
 server.use(express.bodyParser());
 
+server.post('/users/login', function (req, res) {
+	//console.log(JSON.stringify(req.body));
+
+	var email = req.body.email,
+		pswd = req.body.password;
+
+	var user = _.find(fixture.users, function (user) {
+		return user.email == "sandeep.kumar@razorthink.com";
+	});
+
+	if (!_.isUndefined(user) && fixture.users[0].password == pswd) {
+		console.log("User: " + user.fname + " logged in.");
+		res.send("login successful");
+	} else {
+		res.send("No user");
+	}
+
+});
+
 server.listen(8030);
 console.log("Serving static pages on port: 8030");
+
+$.post('/users/login', {
+	email: "Sandeep.asdasd",
+	pswd: "asdasd"
+}, function () {})
